973573f3b88fa85317dee7e5d820d58e73d26db3
Estefany Montiel <amontiel18@alumnos.uaq.mx>
2025-02-27 00:34:12 -0600

    Feat: Cambios

--- PokemonApi/Dtos/HobbiesResponseDto.cs
+++ PokemonApi/Dtos/HobbiesResponseDto.cs
@@ -1,4 +1,5 @@
 using System.Runtime.Serialization;
+<<<<<<< Updated upstream
     using Microsoft.AspNetCore.Authorization.Infrastructure;  //libreria para serializar 
 
     namespace PokemonApi.Dtos; 
@@ -14,6 +15,20 @@ using System.Runtime.Serialization;
         public string Name { get; set; }
         
          [DataMember(Name = "Top", Order = 3)]
+=======
+using Microsoft.AspNetCore.Authorization.Infrastructure;  //libreria para serializar 
+namespace PokemonApi.Dtos; 
+[DataContract(Name = "HobbiesResponseDto", Namespace = "http://pokemon-api/hobbies-service")]
+public class HobbiesResponseDto
+{
+        [DataMember(Name = "Id", Order = 1)]
+        public int Id { get; set; }
+        
+        [DataMember(Name = "Name", Order = 2)]
+        public string Name { get; set; }
+        
+        [DataMember(Name = "Top", Order = 3)]
+>>>>>>> Stashed changes
         public int Top { get; set; }
 
 }
---
+++ PokemonApi/Infrastructure/RelationalDbContext.cs
@@ -0,0 +1,73 @@
+using System.Drawing.Printing;
+using Microsoft.EntityFrameworkCore;
+using PokemonApi.Infrastructure.Entities;
+
+//ENTIDAD CREADA 
+namespace PokemonApi.Infrastructure
+{
+    public class RelationalDbContext : DbContext
+    {
+
+        public  DbSet<PokemonEntity> Pokemons { get; set; } //DbSet es una coleccion de entidades que se mapean a una tabla en la base de datos
+<<<<<<< Updated upstream
+//Constructor  - pasar parametros de conexion al padre que es DbContext     
+=======
+        public  DbSet<HobbyEntity> Hobbies { get; set; } //DbSet es una coleccion de entidades que se mapean a una tabla en la base de datos
+
+        public  DbSet<BookEntity> Books { get; set; } //DbSet es una coleccion de entidades que se mapean a una tabla en la base de datos
+
+        //Constructor  - pasar parametros de conexion al padre que es DbContext     
+>>>>>>> Stashed changes
+        public RelationalDbContext(DbContextOptions<RelationalDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonEntity>(entity=>{
+
+                entity.HasKey(s=>s.Id); //Llave primaria
+                entity.Property(s=>s.Name).IsRequired().HasMaxLength(100); //Nombre es requerido
+                entity.Property(s=>s.Type).IsRequired().HasMaxLength(50); //Tipo es requerido
+                entity.Property(s=>s.Level).IsRequired(); //Nivel es requerido
+                entity.Property(s=>s.attack).IsRequired(); //Ataque es requerido
+                entity.Property(s=>s.Defense).IsRequired(); //Defensa es requerido
+                entity.Property(s=>s.Speed).IsRequired(); //Velocidad es requerido
+<<<<<<< HEAD
+                entity.Property(s=>s.Health).IsRequired(); //Vida es requerido
+
+<<<<<<< Updated upstream
+=======
+>>>>>>> parent of 4d13259 (Merge pull request #5 from EstefanyMontiel/feature_branch_api)
+=======
+            }); 
+
+            modelBuilder.Entity<HobbyEntity>(entity=>{
+
+                entity.HasKey(b=>b.Id); //Llave primaria
+                entity.Property(b=>b.Name).IsRequired().HasMaxLength(100); //Nombre es requerido
+                entity.Property(b=>b.Top).IsRequired(); //Top es requerido
+>>>>>>> Stashed changes
+            }); 
+
+            modelBuilder.Entity<BookEntity>(entity=>{
+
+                entity.HasKey(a=>a.Id); //Llave primaria
+                entity.Property(a=>a.Title).IsRequired().HasMaxLength(100); 
+                entity.Property(a=>a.Author).IsRequired().HasMaxLength(100);  
+                entity.Property(a=>a.PublishedDate).IsRequired(); 
+            }); 
+
+               
+         }
+<<<<<<< Updated upstream
+
+    }
+=======
+         
+//Constructor  - pasar parametros de conexion al padre que es DbContext     
+       
+         }
+>>>>>>> Stashed changes
+   
+}
